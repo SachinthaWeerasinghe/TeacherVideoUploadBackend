9b141b478e3f6aff6f991fdc78c43eb8bb6da907
SachinthaWeerasinghe <weerasi-ps19259@stu.kln.ac.lk>
2024-05-07 09:50:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Teacher-video-upload
+This is the lesson video uploading feature in teacher's side.mvn
+src
+.gitignore
+mvnw
+mvnw.cmd
+pom.xml
---
+++ src/test/java/com/example/videoUploadBackend/FileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videoUploadBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
